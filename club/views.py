5ce4ede752100769fb4ddec750b77ac27f51376e
PavelBoyko07 <pbojko67@gmail.com>
2026-05-23 20:13:58 +0300

    Сделал окно регистрации и вход

--- club/views.py
+++ club/views.py
@@ -2,7 +2,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 from django.shortcuts import get_object_or_404, redirect, render
 from django.shortcuts import render
-
 from .forms import ApplicationForm, LoginForm, RegisterForm, ReviewForm
 from .models import (Application,ApplicationMembership,FitnessDirection,Membership,Review,ScheduleType,Stock,Trainer,TrainingType,Visit,)
 
